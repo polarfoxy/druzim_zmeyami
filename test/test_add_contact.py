298308535a62f5a4e9bdaed290c5b347865b430e
polarfoxy <esaprykina@ourgapps.com>
2018-09-29 14:18:29 +0300

    Реализована загрузка тестовых данных из файла

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from random import randint
-import pytest
-import random
-import string
+
 
 
 
 # @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, json_contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contacts_list()
     app.contact.add(contact)
     assert len(old_contacts) + 1 == app.contact.count()
